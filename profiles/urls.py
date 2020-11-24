c944c148e95a064ba48c08a5b06bb28dad0eba26
Guillermo Brachetta <brachetta@me.com>
2020-11-24 23:09:23 +0100

    Created relationship and Profile managers

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import my_profile_view
+from .views import (
+    invites_received_view,
+    my_profile_view,
+    profiles_list_view,
+    invites_profiles_list_view,
+)
 
 app_name = "profiles"
 
-urlpatterns = [path("myprofile/", my_profile_view, name="my-profile-view")]
+urlpatterns = [
+    path("myprofile/", my_profile_view, name="my-profile-view"),
+    path("my-invites/", invites_received_view, name="my-invites-view"),
+    path("all-profiles/", profiles_list_view, name="all-profiles-view"),
+    path("to-invite/", invites_profiles_list_view, name="to-invite-view"),
+]
