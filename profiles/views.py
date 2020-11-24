3f24b9980d076268b46d3bbecd01613e1215e3c0
Guillermo Brachetta <brachetta@me.com>
2020-11-24 12:59:55 +0100

    Add profile template and main view and urls

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def my_profile_view(request):
+    """Main user view"""
+
+    obj = Profile.objects.get(user=request.user)
+
+    context = {"obj": obj}
+    return render(request, "profiles/my_profile.html", context)
